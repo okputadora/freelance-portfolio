ae1aaabfff8645cca40a8d37b29bffd17be3dff4
okputadora <okputadora@gmail.com>
2018-04-02 14:15:24 -0400

    added sendgrid functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var sgMail = require('@sendgrid/mail');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/:action', function(req, res, next){
+  if (req.params.action === "inquiry"){
+    var params = req.body
+    console.log(params)
+  }
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'mmcveigh33@gmail.com',
+    from: params.email,
+    subject: params.subject,
+    text: params.message
+  }
+  console.log(msg)
+  sgMail.send(msg, function(error, result){
+    if (error){
+      console.log("error")
+      console.log(error)
+      return
+    }
+    else{
+      console.log("success")
+      console.log(result)
+    }
+  })
+})
 
 module.exports = router;
